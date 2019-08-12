5846e86de02b59ab7f2d601d683886257b82aa33
Amil Masic <amil.masic@gmail.com>
2019-08-12 09:34:17 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
-<<<<<<< HEAD
+
   has_many :song_genres
   has_many :genres, through: :song_genres
 
@@ -12,7 +12,3 @@ class Song < ActiveRecord::Base
    Song.all.find{|song| song.slug == slug}
  end
 end
-=======
-  belongs_to :genre
-end
->>>>>>> 13aa1e8fbae138846bfd7b6ecd3a230ebb338de5
