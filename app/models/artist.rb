50dd7a02115dfbca7abf04f77894ef285290fdb4
Amil Masic <amil.masic@gmail.com>
2019-08-12 09:33:48 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,6 @@
 class Artist < ActiveRecord::Base
   has_many :songs
-<<<<<<< HEAD
+
   has_many :genres, :through => :songs
 
 
@@ -11,7 +11,5 @@ class Artist < ActiveRecord::Base
  def self.find_by_slug(slug)
    Artist.all.find{|artist| artist.slug == slug}
  end
-=======
-  has_many :genres, through: :songs
->>>>>>> 13aa1e8fbae138846bfd7b6ecd3a230ebb338de5
+
 end
