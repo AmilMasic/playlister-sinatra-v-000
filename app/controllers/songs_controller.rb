e6437af652402d093807997862355581160606fc
Amil Masic <amil.masic@gmail.com>
2019-08-12 10:07:56 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,7 +6,7 @@ class SongsController < ApplicationController
     erb :'songs/index'
   end
 
-  get 'songs/new' do
+  get '/songs/new' do
       @genres = Genre.all
       @artists = Artist.all
       erb :'songs/new'
